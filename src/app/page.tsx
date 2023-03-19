f8b96eb600c15514170fef56498a063d57310d31
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-19 09:37:38 -0700

    Set up and tested using Link with next router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <h1 className="italic">Hello</h1>
       <p>lets google</p>
+      <Link href="/test">Test Link</Link>
     </div>
   );
 }
