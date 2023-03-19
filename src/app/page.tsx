e4d86ae5af637304c9c5f8d90dc18b223f1eca9b
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-19 09:22:17 -0700

    Modified files, tested prettierrc file and allowed it to update files with semicolons to ensure working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
       <h1 className="italic">Hello</h1>
+      <p>lets google</p>
     </div>
-  )
+  );
 }
