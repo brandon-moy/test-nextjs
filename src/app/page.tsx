bfeeee67cb3d9bc13e65dd4e94321597b13b044e
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-20 11:08:14 -0700

    Fixed timing by uzing arbitrary animaation value including delay - uninstalls animation-delay package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ export default function Home() {
     <div className="bg-[#354a4a] w-full h-screen flex flex-col items-center justify-center">
       <h1 className="font-light text-white text-7xl">Brandon Moy</h1>
       <div className="relative flex justify-center w-full pt-4">
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-cycle animation-delay-none">
+        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite]">
           Software Engineer
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-cycle animation-delay-4000">
+        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_4s]">
           CSS Artist
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-cycle animation-delay-8000">
+        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_8s]">
           Front-End Developer
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-cycle animation-delay-12000">
+        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_12s]">
           Programmer
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-cycle animation-delay-16000">
+        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_16s]">
           Web Developer
         </span>
       </div>
