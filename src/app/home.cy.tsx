ec64e9f044d8fa3e12cf1f59e67cd1b88d0d9854
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-05-31 10:27:04 -0700

    Applied Cypress best practices of using data-cy attributes to better isolate elements on home.cy.tsx and page.tsx

--- src/app/home.cy.tsx
+++ src/app/home.cy.tsx
@@ -14,33 +14,39 @@ describe("Renders the Home application page", () => {
 
     it("Renders the navbar", () => {
       it("Render links on the navbar", () => {
-        cy.get("#home-link").should("be.visible").and("have.attr", "href", "/");
-        cy.get("#about-link")
+        cy.get("[data-cy='home-link']")
+          .should("be.visible")
+          .and("have.attr", "href", "/");
+        cy.get("[data-cy='about-link']")
           .should("be.visible")
           .and("have.attr", "href", "/about-me");
-        cy.get("#projects-link")
+        cy.get("[data-cy='projects-link']")
           .should("be.visible")
           .and("have.attr", "href", "/projects");
-        cy.get("#skills-link")
+        cy.get("[data-cy='skills-link']")
           .should("be.visible")
           .and("have.attr", "href", "/skills");
       });
     });
 
     it("Renders the name", () => {
-      cy.get("#header-name").should("be.visible");
+      cy.get("[data-cy='header-name']").should("be.visible");
     });
 
     it("Checks the animations", () => {
-      cy.contains("Software Engineer")
+      cy.get("[data-cy='animate-software-engineer']")
         .should("be.visible")
         .wait(4000, { log: false });
-      cy.contains("CSS Artist").should("be.visible").wait(4000, { log: false });
-      cy.contains("Front-End Developer")
+      cy.get("[data-cy='animate-css-artist']")
         .should("be.visible")
         .wait(4000, { log: false });
-      cy.contains("Programmer").should("be.visible").wait(4000, { log: false });
-      cy.contains("Web Developer")
+      cy.get("[data-cy='animate-front-end-dev']")
+        .should("be.visible")
+        .wait(4000, { log: false });
+      cy.get("[data-cy='animate-prog']")
+        .should("be.visible")
+        .wait(4000, { log: false });
+      cy.get("[data-cy='animate-web-dev']")
         .should("be.visible")
         .wait(4000, { log: false });
     });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,38 @@ export default function Home() {
   return (
     <div className="bg-[#354a4a] w-full h-screen flex flex-col items-center justify-center">
       <div className="absolute w-24 h-24 bg-white rounded-full animate-bounce top-32 left-24"></div>
-      <h1 id="header-name" className="font-light text-white text-7xl">
+      <h1 data-cy="header-name" className="font-light text-white text-7xl">
         Brandon Moy
       </h1>
       <div className="relative flex justify-center w-full pt-4">
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite]">
+        <span
+          data-cy="animate-software-engineer"
+          className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite]"
+        >
           Software Engineer
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_4s]">
+        <span
+          data-cy="animate-css-artist"
+          className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_4s]"
+        >
           CSS Artist
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_8s]">
+        <span
+          data-cy="animate-front-end-dev"
+          className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_8s]"
+        >
           Front-End Developer
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_12s]">
+        <span
+          data-cy="animate-prog"
+          className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_12s]"
+        >
           Programmer
         </span>
-        <span className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_16s]">
+        <span
+          data-cy="animate-web-dev"
+          className="absolute text-2xl font-bold text-white opacity-0 animate-[cycle_20s_linear_infinite_16s]"
+        >
           Web Developer
         </span>
       </div>
