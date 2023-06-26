ef866df13b4994b695a1d74857dcf3402e2f382f
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-06-26 12:05:44 -0700

    Started to create layout for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,27 @@
 export default function Projects() {
-  return <div>I'm a project</div>;
+  return (
+    <div className="flex flex-wrap w-full sm:h-full lg:h-screen bg-main">
+      <div className="flex flex-wrap w-full p-4 mt-16">
+        <div className="basis-2/5"></div>
+        <div className="text-white basis-3/5">
+          <p>Mission Beepossible</p>
+          <p>Full-Stack Web Application</p>
+          <p className="italic">
+            A full-stack Next.js (w/TypeScript) application for future
+            astronauts to learn about the universe.
+          </p>
+          <p>
+            This application was developed for the Dear Junior Dev Quackathon
+            prompt: A full-stack web application to teach users about the solar
+            system. The tech stack used for this application was Next.js,
+            TypeScript, React, TailwindCSS, CSS3, Docker, PostgreSQL, Prisma,
+            Supabase, and Vercel. Partner project with{" "}
+            <a href="https://github.com/cam-peck" className="underline">
+              Cameron Peck
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
